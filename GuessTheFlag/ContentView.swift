17d25d70fca129fa5e6924789993800eafddfa40
Leo Chung <leo.chung97@gmail.com>
2023-12-15 13:23:53 -0500

    Animation challenges

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    @State private var opacity = 1.0
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
-
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
             ], center: .top, startRadius: 200, endRadius: 400)
-                .ignoresSafeArea()
+            .ignoresSafeArea()
             
             VStack {
                 Text("Guess the Flag")
@@ -41,18 +43,21 @@ struct ContentView: View {
                     
                     ForEach(0..<3) { number in
                         Button {
-                           flagTapped(number)
+                            flagTapped(number)
+
                         } label: {
                             Image(countries[number])
                                 .clipShape(.capsule)
                                 .shadow(radius: 5)
                         }
+                        .opacity(opacity)
+                        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
                     }
                 }
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 20)
-                    .background(.regularMaterial)
-                    .clipShape(.rect(cornerRadius: 20))
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
                 
                 Spacer()
                 
@@ -62,7 +67,7 @@ struct ContentView: View {
                 
                 Spacer()
             }
-                .padding()
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
@@ -76,8 +81,14 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            withAnimation {
+                animationAmount += 360
+            }
         } else {
             scoreTitle = "Wrong, that's the flag of \(countries[number])"
+            withAnimation {
+                opacity = 0.25
+            }
         }
         showingScore = true
     }
@@ -85,6 +96,7 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        opacity = 1
     }
 }
 
